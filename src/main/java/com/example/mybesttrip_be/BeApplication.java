58aa7c23ccef07cd8cc0cb15b6afa435ad3610c9
naimkim <naimk5451@gmail.com>
2025-09-09 17:27:10 +0900

    refrac: app 이름 변경 be -> mybesttripbe

--- src/main/java/com/example/mybesttrip_be/BeApplication.java
+++ src/main/java/com/example/mybesttrip_be/BeApplication.java
@@ -1,4 +1,4 @@
-package com.example.be;
+package com.example.mybesttrip_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/mybesttrip_be/BeApplicationTests.java
+++ src/test/java/com/example/mybesttrip_be/BeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.be;
+package com.example.mybesttrip_be;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
